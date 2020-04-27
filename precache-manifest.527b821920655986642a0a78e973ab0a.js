1c9678be4b5ec113d9cd53c625ae24e4bb7c17a6
Eoin <me@eoinfeely.com>
2020-04-27 11:13:25 +0100

    Updates

---
+++ precache-manifest.527b821920655986642a0a78e973ab0a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc09aa4c1367e2dadfe2b90321a10f3",
+    "url": "/osterialavignetta/index.html"
+  },
+  {
+    "revision": "0b667738eaa7858f9df4",
+    "url": "/osterialavignetta/static/css/main.d2167c61.chunk.css"
+  },
+  {
+    "revision": "0a09daa63814c53ce8e5",
+    "url": "/osterialavignetta/static/js/2.403e060f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/osterialavignetta/static/js/2.403e060f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b667738eaa7858f9df4",
+    "url": "/osterialavignetta/static/js/main.afa56005.chunk.js"
+  },
+  {
+    "revision": "77d9f3051b1b0d1fa4fd",
+    "url": "/osterialavignetta/static/js/runtime-main.9162338f.js"
+  },
+  {
+    "revision": "173ca207f957ea9ce619f35959d645a3",
+    "url": "/osterialavignetta/static/media/chef-1.173ca207.png"
+  },
+  {
+    "revision": "80f600b82bb09e98391903f0e92d4773",
+    "url": "/osterialavignetta/static/media/chef-2.80f600b8.png"
+  },
+  {
+    "revision": "8056a0e691b56f2c3522d7ba77c54f2f",
+    "url": "/osterialavignetta/static/media/chef-3.8056a0e6.png"
+  },
+  {
+    "revision": "36dc74bf8532d2b2cfb4243951333fce",
+    "url": "/osterialavignetta/static/media/chef-4.36dc74bf.png"
+  },
+  {
+    "revision": "d9bb33d5800d4979b2477820ec5728d0",
+    "url": "/osterialavignetta/static/media/fb_logo_grey.d9bb33d5.svg"
+  },
+  {
+    "revision": "5bfa8a7d81f208f1587cf705dcfde444",
+    "url": "/osterialavignetta/static/media/fb_logo_yellow.5bfa8a7d.svg"
+  },
+  {
+    "revision": "1d1fd54cbaa2bc925e4407a0dfeaeb85",
+    "url": "/osterialavignetta/static/media/food_bg.1d1fd54c.svg"
+  },
+  {
+    "revision": "47a22a9830e5f438a4258e5f45f55d50",
+    "url": "/osterialavignetta/static/media/food_bg_02.47a22a98.svg"
+  },
+  {
+    "revision": "c2631fe6153d8a20fc008934ed0e4029",
+    "url": "/osterialavignetta/static/media/inside@2x.c2631fe6.jpg"
+  },
+  {
+    "revision": "043485d046b18149aeee38f70d50d352",
+    "url": "/osterialavignetta/static/media/insta_logo_grey.043485d0.svg"
+  },
+  {
+    "revision": "b5b5a5f9afb817e941f6733042a1f986",
+    "url": "/osterialavignetta/static/media/insta_logo_yellow.b5b5a5f9.svg"
+  },
+  {
+    "revision": "aa8bd72341a6670dab5516f476f16b66",
+    "url": "/osterialavignetta/static/media/meat@2x.aa8bd723.jpg"
+  },
+  {
+    "revision": "e994e758cceb71c7e7d029444bf55c10",
+    "url": "/osterialavignetta/static/media/meat_2@2x.e994e758.jpg"
+  },
+  {
+    "revision": "b0bf77f69ec7bc40f8bcd6010f60d442",
+    "url": "/osterialavignetta/static/media/mightype_script.b0bf77f6.otf"
+  },
+  {
+    "revision": "3886ad8d70ab5e56a8b34fe96060706e",
+    "url": "/osterialavignetta/static/media/osteria_grapes_grey.3886ad8d.svg"
+  },
+  {
+    "revision": "3306825f488eb1e1a170d7d9c14c3acb",
+    "url": "/osterialavignetta/static/media/osteria_logo_yellow.3306825f.svg"
+  },
+  {
+    "revision": "414bcb0d29dafe9d92b737bda86de08d",
+    "url": "/osterialavignetta/static/media/ravioli@2x.414bcb0d.jpg"
+  },
+  {
+    "revision": "57312629522a50b904497078a16fa8b2",
+    "url": "/osterialavignetta/static/media/risotto@2x.57312629.jpg"
+  },
+  {
+    "revision": "ae515300ec8629e3a0204a538cf25ecd",
+    "url": "/osterialavignetta/static/media/risotto_cut.ae515300.png"
+  },
+  {
+    "revision": "a221667fbdfd9204d3a32491a7e25af7",
+    "url": "/osterialavignetta/static/media/wine_table@2x.a221667f.jpg"
+  }
+]);
